0ec5b5bb082acc4a22bde0a09f7f5d6537811a95
H1rono <hronok66@gmail.com>
2024-01-09 19:10:55 +0900

    Fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use mini_tokio::{Delay, MiniTokio};
 
 fn main() {
     let mut mini_tokio = MiniTokio::new();
-    for i in 1..=1 {
+    for i in 1..=3 {
         mini_tokio.spawn(async move {
             let delay = Delay::after(Duration::from_secs(i));
             let out = delay.await;
